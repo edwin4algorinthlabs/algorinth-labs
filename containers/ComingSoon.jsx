b9b3a0a2e43298fa730e045002c9dc29a67ac004
CyberGA <etomu.joshua@gmail.com>
2023-08-02 23:47:03 +0100

    fix scaling issue

--- containers/ComingSoon.jsx
+++ containers/ComingSoon.jsx
@@ -3,9 +3,9 @@ import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram, FaDribbble } from "r
 
 export default function ComingSoonContainer() {
   return (
-    <div className="h-screen relative z-0 w-full bg-[#08111F] bg-[url('/images/coming.png')] bg-contain bg-no-repeat bg-center">
+    <div className="h-screen relative z-0 w-full bg-[#08111F] bg-[url('/images/coming.png')] bg-contain bg-no-repeat bg-center overflow-hidden">
       <div className="flex w-full mx-auto">
-        <Image src="/images/logo.png" width={310} height={220} alt="Brand logo" placeholder="blur" blurDataURL="data" priority className="w-[310px]" />
+        <Image src="/images/logo.png" width={310} height={220} alt="Brand logo" placeholder="blur" blurDataURL="data" priority className="max-w-[21.5vw] max-h-[21.4vh] object-contain" />
         <div className="flex flex-1 justify-center max-w-[70%]">
           <p className="text-[64px] text-center text-white leading-[96px] font-poppins font-thin ">Launching Soon...</p>
         </div>
@@ -13,15 +13,15 @@ export default function ComingSoonContainer() {
 
       {/* Email form */}
       <div className="w-full absolute z-10 bottom-[3.14%] flex justify-center">
-        <div className="max-w-[48%] w-full flex flex-row h-[72.1px] rounded-[10px] bg-white ">
+        <div className="max-w-[52vw] w-full flex flex-row h-[72.1px] rounded-[10px] bg-white ">
           <input type="email" placeholder="Email Address" className="w-full h-[72.1px] p-4 rounded-[10px] outline-0 border-none text-black text-[28px] font-normal font-poppins placeholder:text-black/60 placeholder:text-[28px] placeholder:font-normal" />
           <button className="h-[72.1px] w-full max-w-[246.81px] bg-secondary rounded-r-[10px] text-[32px] font-poppins font-normal">Notify Me</button>
         </div>
       </div>
 
       {/* Balls */}
-      <div className="fixed top-[34.5%] left-[-40%] z-[1] w-[50.85%] h-[68.2%] rounded-full gradient1"></div>
-      <div className="flex flex-col justify-center gap-6 fixed top-[34.5%] right-[-40%] z-[1] w-[50.85%] h-[68.2%] rounded-full gradient2 pl-[92px]">
+      <div className=" absolute bottom-[-3.2vh] left-[-52vh] z-[1] w-[73.632vh] h-[73.632vh] rounded-full gradient1"></div>
+      <div className="flex flex-col justify-center gap-6 absolute bottom-[-3.2vh] right-[-52vh] z-[1] w-[73.632vh] h-[73.632vh] rounded-full gradient2 pl-[92px]">
         <FaFacebookF color="#fff" size={30} />
         <FaTwitter color="#fff" size={30} />
         <FaLinkedinIn color="#fff" size={30} />
@@ -30,4 +30,4 @@ export default function ComingSoonContainer() {
       </div>
     </div>
   );
-}
+} 
