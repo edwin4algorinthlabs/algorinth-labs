0721cace82b17bdd074db230290d0f0a42cacb86
CyberGA <etomu.joshua@gmail.com>
2023-08-02 21:17:42 +0100

    fixing bug

--- containers/ComingSoon.jsx
+++ containers/ComingSoon.jsx
@@ -12,18 +12,16 @@ export default function ComingSoonContainer() {
       </div>
 
       {/* Email form */}
-      <div className="w-full absolute z-10 bottom-[34px] flex justify-center">
-        <div className="max-w-[812px] w-full flex flex-row h-[72.1px] rounded-[10px] bg-white ">
+      <div className="w-full absolute z-10 bottom-[3.14%] flex justify-center">
+        <div className="max-w-[48%] w-full flex flex-row h-[72.1px] rounded-[10px] bg-white ">
           <input type="email" placeholder="Email Address" className="w-full h-[72.1px] p-4 rounded-[10px] outline-0 border-none text-black text-[28px] font-normal font-poppins placeholder:text-black/60 placeholder:text-[28px] placeholder:font-normal" />
-          <button className="h-[72.1px] w-full max-w-[246.81px] bg-secondary rounded-r-[10px] text-[32px] font-poppins font-normal">
-            Notify Me
-          </button>
+          <button className="h-[72.1px] w-full max-w-[246.81px] bg-secondary rounded-r-[10px] text-[32px] font-poppins font-normal">Notify Me</button>
         </div>
       </div>
 
       {/* Balls */}
-      <div className="fixed bottom-[-30px] left-[-533px] z-[1] w-[754px] h-[769px] rounded-full gradient1"></div>
-      <div className="flex flex-col justify-center gap-6 fixed bottom-[-30px] right-[-533px] z-[1] w-[754px] h-[769px] rounded-full gradient2 pl-[92px]">
+      <div className="fixed top-[34.5%] left-[-40%] z-[1] w-[50.85%] h-[68.2%] rounded-full gradient1"></div>
+      <div className="flex flex-col justify-center gap-6 fixed top-[34.5%] right-[-40%] z-[1] w-[50.85%] h-[68.2%] rounded-full gradient2 pl-[92px]">
         <FaFacebookF color="#fff" size={30} />
         <FaTwitter color="#fff" size={30} />
         <FaLinkedinIn color="#fff" size={30} />
