66daec673049b5028854c3c2bb750422f4e90af5
CyberGA <etomu.joshua@gmail.com>
2023-08-03 22:13:02 +0100

    UI modified

--- containers/ComingSoon.jsx
+++ containers/ComingSoon.jsx
@@ -3,30 +3,30 @@ import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram, FaDribbble } from "r
 
 export default function ComingSoonContainer() {
   return (
-    <div className="h-screen relative z-0 w-full bg-[#08111F] bg-[url('/images/coming.png')] bg-contain bg-no-repeat bg-center overflow-hidden">
+    <div className="h-screen relative z-0 w-full bg-[#08111F] bg-[url('/images/coming.png')] bg-contain bg-no-repeat bg-center overflow-hidden aspect-none">
       <div className="flex w-full mx-auto">
         <Image src="/images/logo.png" width={310} height={220} alt="Brand logo" placeholder="blur" blurDataURL="data" priority className="max-w-[21.5vw] max-h-[21.4vh] object-contain" />
-        <div className="flex flex-1 justify-center max-w-[70%]">
-          <p className="text-[64px] text-center text-white leading-[96px] font-poppins font-thin ">Launching Soon...</p>
+        <div className="flex flex-1 justify-center max-w-[65vw]">
+          <p className="text-[54px] text-center text-white leading-[96px] font-poppins font-thin ">Launching Soon...</p>
         </div>
       </div>
 
       {/* Email form */}
       <div className="w-full absolute z-10 bottom-[3.14%] flex justify-center">
         <div className="max-w-[52vw] w-full flex flex-row h-[72.1px] rounded-[10px] bg-white ">
-          <input type="email" placeholder="Email Address" className="w-full h-[72.1px] p-4 rounded-[10px] outline-0 border-none text-black text-[28px] font-normal font-poppins placeholder:text-black/60 placeholder:text-[28px] placeholder:font-normal" />
-          <button className="h-[72.1px] text-white w-full max-w-[246.81px] bg-secondary rounded-r-[10px] text-[32px] font-poppins font-normal">Notify Me</button>
+          <input type="email" placeholder="Email Address" className="w-full h-[72.1px] p-4 rounded-[10px] outline-0 border-none text-black text-2xl font-normal font-poppins placeholder:text-black/60 placeholder:text-2xl placeholder:font-normal" />
+          <button className="h-[72.1px] text-white w-full max-w-[246.81px] bg-secondary/80 hover:bg-secondary transition duration-300 ease-in-out delay-75 rounded-r-[10px] text-2xl font-poppins font-normal">Notify Me</button>
         </div>
       </div>
 
       {/* Balls */}
       <div className=" absolute bottom-[-3.2vh] left-[-52vh] z-[1] w-[73.632vh] h-[73.632vh] rounded-full gradient1"></div>
-      <div className="flex flex-col justify-center gap-6 absolute bottom-[-3.2vh] right-[-52vh] z-[1] w-[73.632vh] h-[73.632vh] rounded-full gradient2 pl-[6%]">
-        <FaFacebookF color="#fff" size={30} />
-        <FaTwitter color="#fff" size={30} />
-        <FaLinkedinIn color="#fff" size={30} />
-        <FaInstagram color="#fff" size={30} />
-        <FaDribbble color="#fff" size={30} />
+      <div className="flex flex-col justify-center gap-7 absolute bottom-[-3.2vh] right-[-52vh] z-[1] w-[73.632vh] h-[73.632vh] rounded-full gradient2 pl-[5.2%]">
+        {/* <FaFacebookF color="#fff" size={30} /> */}
+        {/* <FaTwitter color="#fff" size={30} className="cursor-pointer" /> */}
+        <FaLinkedinIn color="#fff" size={30} className="cursor-pointer hover:scale-125 transition duration-300 ease-in-out delay-75" />
+        <FaInstagram color="#fff" size={30} className="cursor-pointer hover:scale-125 transition duration-300 ease-in-out delay-75" />
+        {/* <FaDribbble color="#fff" size={30} /> */}
       </div>
     </div>
   );
