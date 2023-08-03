6a899730de621e7161934e2da967b0249b9a9f39
prcabrah <abrahametomu@yahoo.com>
2023-08-03 12:25:57 +0100

    fix scaling

--- containers/ComingSoon.jsx
+++ containers/ComingSoon.jsx
@@ -15,13 +15,13 @@ export default function ComingSoonContainer() {
       <div className="w-full absolute z-10 bottom-[3.14%] flex justify-center">
         <div className="max-w-[52vw] w-full flex flex-row h-[72.1px] rounded-[10px] bg-white ">
           <input type="email" placeholder="Email Address" className="w-full h-[72.1px] p-4 rounded-[10px] outline-0 border-none text-black text-[28px] font-normal font-poppins placeholder:text-black/60 placeholder:text-[28px] placeholder:font-normal" />
-          <button className="h-[72.1px] w-full max-w-[246.81px] bg-secondary rounded-r-[10px] text-[32px] font-poppins font-normal">Notify Me</button>
+          <button className="h-[72.1px] text-white w-full max-w-[246.81px] bg-secondary rounded-r-[10px] text-[32px] font-poppins font-normal">Notify Me</button>
         </div>
       </div>
 
       {/* Balls */}
       <div className=" absolute bottom-[-3.2vh] left-[-52vh] z-[1] w-[73.632vh] h-[73.632vh] rounded-full gradient1"></div>
-      <div className="flex flex-col justify-center gap-6 absolute bottom-[-3.2vh] right-[-52vh] z-[1] w-[73.632vh] h-[73.632vh] rounded-full gradient2 pl-[92px]">
+      <div className="flex flex-col justify-center gap-6 absolute bottom-[-3.2vh] right-[-52vh] z-[1] w-[73.632vh] h-[73.632vh] rounded-full gradient2 pl-[6%]">
         <FaFacebookF color="#fff" size={30} />
         <FaTwitter color="#fff" size={30} />
         <FaLinkedinIn color="#fff" size={30} />
