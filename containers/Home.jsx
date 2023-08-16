f4630570e49a909ae976d71fda2f6a4c4532db7d
Adeola <aadelakun28@gmail.com>
2023-08-16 13:49:57 +0100

    Added underlying background

--- containers/Home.jsx
+++ containers/Home.jsx
@@ -3,34 +3,34 @@ import Image from 'next/image';
 
 const Home = () => {
   return (
-    <div className="div-hero bg-cover bg-center h-screen flex items-center justify-center">
-      <div className="div-w-container max-w-screen-xl h-screen relative">
-        <p className="heading-we-don-t absolute top-32 left-0 text-white font-bold text-3xl md:text-4xl xl:text-5xl">
+    <div className="outerContainer">
+    <div className="div-hero">
+      <div className="div-w-container div-w-container-ii">
+        <p className="heading-we-don-t div-w-container-heading">
           We fix your contract, so you can focus on productivity.
         </p>
-        <div className="p-newsletter absolute top-2/4 left-0 transform -translate-y-1/2 px-8">
-          <p className="algorinth-labs-is text-white font-normal text-lg md:text-xl xl:text-2xl">
+        <div className="p-newsletter">
+          <p className="algorinth-labs-is">
             Algorinth Labs is dedicated to ensuring the integrity of blockchain ecosystems by providing meticulous smart contract audits,
             <br />
             rigorous testing, and innovative software development.
           </p>
         </div>
-        <div className="link absolute top-2/3 left-0 transform -translate-y-1/2 bg-blue-500 border border-black rounded-full px-8 py-2">
-          <div className="request-an-AUDIT text-white font-medium text-base md:text-lg xl:text-xl">
+        <div className="link">
+          <div className="request-an-AUDIT">
             REQUEST AN AUDIT
           </div>
         </div>
-        <div className="overlap-group absolute top-0 left-0 w-full h-52 md:h-60">
-          <div className="banner w-full h-full relative">
+        <div className="overlap-group">
+          <div className="banner">
             <Image
-              className="link-home-trail-of absolute top-2/4 transform -translate-y-2/4"
+              className="link-home-trail-of"
               alt="Link home trail of"
               src="/images/link-home-trail-of-bits-svg.svg.png"
               width={32}
               height={13}
             />
             <div className="navbar absolute top-0 left-36 w-full h-full flex items-center space-x-4 text-white font-medium text-base md:text-lg xl:text-xl">
-              {/* <div className="div-wrapper border-b-2 border-red-600 py-2"> </div> */}
               <div>Home</div>
               <div>Services</div>
               <div>Resources</div>
@@ -48,6 +48,7 @@ const Home = () => {
           />
         </div>
       </div>
+    </div>
     </div>
   );
 };
