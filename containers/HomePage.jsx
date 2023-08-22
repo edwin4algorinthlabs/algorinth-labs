79f26e2329f2bed6a57db2b1c8916abc31ec3475
Adeola <aadelakun28@gmail.com>
2023-08-22 21:20:38 +0100

    Starting separate components for mobile

--- containers/HomePage.jsx
+++ containers/HomePage.jsx
@@ -1,16 +1,34 @@
+"use client"
 import React from "react";
+import { useEffect, useState } from 'react';
 import Header from "../components/header-component/Header";
 import ServiceOverview from "../components/service-overview-component/ServiceOverview";
-import Rider from "../components/call-to-action-component/CTA";
+import CTAMobile from "../components/call-to-action-component/CTAMobile";
+import CTA from "../components/call-to-action-component/CTA";
 import Footer from "../components/footer-component/Footer";
 import DisplayPrices from "../components/price-display-component/DisplayPrices";
 
 const HomePage = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth <= 768);
+    }
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
+    
     <div className="">
+      {/* {isMobile ? <CTAMobile /> : <CTA />} */}
       <Header />
       <ServiceOverview />
-      <Rider />
+      <CTA />
       <DisplayPrices />
       <Footer />
     </div>
