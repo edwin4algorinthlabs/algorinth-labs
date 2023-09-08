9ae74544f3f3b5c8ac7d730d9dd1a92ee273568b
Adeola <aadelakun28@gmail.com>
2023-09-08 13:09:25 +0100

    Update Footer

--- components/mobile-footer-component/MobileFooter.js
+++ components/mobile-footer-component/MobileFooter.js
@@ -12,7 +12,7 @@ const MobileFooter = () => {
                         development, driving the evolution of decentralized technologies.
       </p>
       <div className="text-[#8294a3] text-base font-medium tracking-[2px] h-auto left-[50%] transform translate-x-[-50%] absolute top-[250px]">
-        CONNECT WITH US
+        <p className="flex justify-center">CONNECT WITH US</p>
         <div className="grid pl-10 grid-cols-2 mt-8">
                         <div className="col-span-1">
                         <div className="inline-flex absolute text-xs px-2 pb-1">
@@ -20,11 +20,34 @@ const MobileFooter = () => {
                             height={30} />                            
                             <div className="text-[#8294a3] px-2 text-sm font-semibold relative tracking-normal leading-5 -mt-px whitespace-no-wrap">Twitter</div>
                             </div>
-
+                            <div className="inline-flex mt-10 px-2 pb-1">
+                            <Image className="img h-5 w-5" alt="Twitter alt svg" src="/images/linkedin-square-svg.svg" width={30}
+                            height={30} />                            
+                            <div className="text-[#8294a3] px-2 text-sm font-semibold relative tracking-normal leading-5 -mt-px whitespace-no-wrap">LinkedIn</div>
                             </div>
+                            <div className="inline-flex mt-5 px-2 pb-1">
+                            <Image className="img h-5 w-5" alt="Twitter alt svg" src="/images/github-svg.svg" width={145}
+                            height={129} />                            
+                            <div className="text-[#8294a3] px-2 text-sm font-semibold relative tracking-normal leading-5 -mt-px whitespace-no-wrap">GitHub</div>
+                            </div>
+                            </div>
+
+
                             <div className="col-span-1">
+                            <div className="px-2 text-xs pb-3">
+                            <div className="text-[#8294a3] px-2 text-sm font-semibold relative tracking-normal leading-5 -mt-px whitespace-no-wrap">Careers</div>
+                            </div>
+                            <div className="px-2 pb-3">
+                            <div className="text-[#8294a3] px-2 text-sm font-semibold relative tracking-normal leading-5 -mt-px whitespace-no-wrap">Resources</div>
+                            </div>
+                            <div className="px-2 pb-3">
+                            <div className="text-[#8294a3] px-2 text-sm font-semibold relative tracking-normal leading-5 -mt-px whitespace-no-wrap">Blog</div>
+                            </div>
 
                             </div>
+                            {/* <div className="text-[#8294a3] text-base font-medium tracking-[2px] h-auto left-[50%] transform translate-x-[-50%] absolute top-[190px]">
+        <p className="flex justify-center">JOIN OUR NEWSLETTER</p>
+      </div> */}
                             
                             
                         
