de09a8ce8bcb4ff1b7c5dd5284c6ae1b59f99f03
Adeola <aadelakun28@gmail.com>
2023-09-15 17:53:48 +0100

    pages routed

---
+++ app/services/page.js
@@ -0,0 +1,38 @@
+"use client"
+import React from "react";
+import { useEffect, useState } from 'react';
+import Header from "../../components/header-component/SharedHeader";
+import  MobileHeader from "../../components/mobile-header-component/MobileHeader";
+import Footer from "../../components/footer-component/Footer";
+import MobileFooter from "../../components/mobile-footer-component/MobileFooter";
+import DisplayPrices from "../../components/price-display-component/DisplayPrices";
+import Background from "../../components/background-component/Background";
+import MobileBackground from "../../components/background-component/MobileBackground";
+
+
+const Services = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth <= 640);
+    }
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  return (
+    
+    <div className="">
+      {isMobile ? <MobileHeader /> : <Header />}
+      {isMobile ? <MobileBackground /> : <Background />}
+      <DisplayPrices />
+      {isMobile? <MobileFooter /> : <Footer />}
+    </div>
+  );
+};
+
+export default Services;
