8aaf09d8ff1e443b2e3c2aa1a1f4328d67bced0a
soloexcel <solomonlekan1@gmail.com>
2023-10-31 16:23:29 +0100

    updated the blog api

---
+++ pages/api/blog.js
@@ -0,0 +1,26 @@
+"use client"
+import React from "react"
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { useEffect,useState } from "react";
+const contentful = require("contentful")
+const client = contentful.createClient({
+    space: 'hxqm2du3oh7j',
+    environment: 'master', // defaults to 'master' if not set
+    accessToken: 'O6DlrG-NCdovo84zhtSiMizryFQBUKCGTbElzbqdhfA'
+  })
+
+  export default async (req, res) => {
+   let richText = null
+   try{
+
+    await client.getEntries().then((entries) => {
+        richText = entries.items
+      })
+      .catch(console.error)
+     console.log("hello world")
+     res.json({ message: "Blog content fetched successfully", richText });
+   }catch (error) {
+   console.error(error);
+   res.status(500).json({ error: "Internal Server Error" });
+ }
+ }
