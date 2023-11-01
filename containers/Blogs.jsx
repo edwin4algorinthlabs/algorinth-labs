8ea50bb9587664be8535a4fb9c03a9b6064940e7
jupiterBill <techwizardhyped@gmail.com>
2023-11-01 05:08:20 +0100

    latest changes

--- containers/Blogs.jsx
+++ containers/Blogs.jsx
@@ -2,6 +2,7 @@
 import RequestDemo from '@/components/shared/request';
 import axios from 'axios';
 import { useEffect,useState} from 'react';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 export default function BlogsContainer() {
     const[blogData,setBlogData] = useState(null)
     useEffect(()=>{
@@ -14,7 +15,7 @@ export default function BlogsContainer() {
           const response =await axios.get('/api/blog')
           console.log(response.data.richText)
           setBlogData(response.data.richText)
-          console.log(blogData)
+          console.log(type(blogData))
 
         }catch(error){
           console.log(error)
@@ -25,22 +26,31 @@ export default function BlogsContainer() {
         <h3 className="text-center text-2xl sm:text-5xl font-bold my-10 sm:my-20">Algorinth Labs Blog</h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-6 gap-y-8 w-full max-w-[1244px] mx-auto duration-300">
             {/* =============== Blog =============== */}
-            <div className="flex flex-col w-full max-w-[602px]">
-                <div className="bg-[#141414] w-full h-[300px] rounded-lg"></div>
-                <h2 className="text-xl sm:text-[32px] font-bold pb-1 sm:pb-2 pt-4 sm:pt-6">{blogData ? blogData[0].fields.title.content[0].content[0].value:"Loading"}</h2>
+            {blogData ? blogData.map((item,index)=>(
+              
+                <div className="flex flex-col w-full max-w-[602px]">
+                <div className="bg-[#141414] w-full h-[300px] rounded-lg">
+                  <img src={item.fields.imageAPI.fields.file.url} width={500} height={650}/>
+                </div>
+                <h2 className="text-xl sm:text-[32px] font-bold pb-1 sm:pb-2 pt-4 sm:pt-6">{item ? documentToReactComponents(item.fields.title):"loading..."}</h2>
                 <p className="text-xs sm:text-xl text-[#525252]">More details on it</p>
-                <p className="text-base sm:text-2xl py-4 sm:py-6">{blogData ? blogData[0].fields.content.content[0].content[1].value.slice(0,100)+"...":"Loading"}</p>
-                <p className="text-xs sm:text-xl text-[#525252]">Aisha Faruq. 28 Aug, 2023</p>
-            </div>
+                <p className="text-base sm:text-2xl py-4 sm:py-6">{item ? documentToReactComponents(item.fields.content)[0]:"Loading"}</p>
+                <p className="text-xs sm:text-xl text-[#525252]">{item? item.fields.author: "..."} . {new Date(item.sys.updatedAt).toLocaleString()}</p>
+                </div>
+
+            )): <div className="flex flex-col w-full max-w-[602px]">
+              <p>Loading...</p>
+              <div className="bg-[#141414] w-full h-[300px] rounded-lg"></div>
+          </div>}
+           
             {/* =============== Blog =============== */}
-            <div className="flex flex-col w-full max-w-[602px]">
+            {/*<div className="flex flex-col w-full max-w-[602px]">
                 <div className="bg-[#141414] w-full h-[300px] rounded-lg"></div>
                 <h2 className="text-xl sm:text-[32px] font-bold pb-1 sm:pb-2 pt-4 sm:pt-6">{blogData ? blogData[1].fields.title.content[0].content[0].value:"Loading"}</h2>
                 <p className="text-xs sm:text-xl text-[#525252]">More details on it</p>
-                <p className="text-base sm:text-2xl py-4 sm:py-6">Body text  text  text  text  text  text  text  
-                text  text text text text text text text text text text text text text text text text.t</p>
-                <p className="text-xs sm:text-xl text-[#525252]">Aisha Faruq. 28 Aug, 2023</p>
-            </div>
+                <p className="text-base sm:text-2xl py-4 sm:py-6">The importanta of smart contract security</p>
+                <p className="text-xs sm:text-xl text-[#525252]">{blogData? blogData[1].fields.author: "..."} 28 Aug, 2023</p>
+              </div>*/}
         </div>
         <div className="flex items-center justify-center">
             <button type="button" className="h-[54px] w-[330px] sm:h-20 text-primary text-base sm:text-2xl font-semibold border-[3px] border-primary rounded-lg mt-20 sm:mt-[100px] mb-20">Load More</button>
