a0e4331f1f97f535d2a9993e343f30777289cd56
soloexcel <solomonlekan1@gmail.com>
2023-11-01 17:23:25 +0100

    fixed the environment variables

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -1,45 +1,25 @@
-<<<<<<< HEAD
-const contentful = require("contentful")
-const client = contentful.createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    environment: 'master', // defaults to 'master' if not set
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
-  })
-
-  // export default async  (req, res) => {
-  export default async function Blog (req, res) {
-  
-=======
 "use client"
 import React from "react"
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { useEffect,useState } from "react";
 const contentful = require("contentful")
 const client = contentful.createClient({
-    space: 'hxqm2du3oh7j',
+    space: process.env.NEXT_PUBLIC_SPACE_ID,
     environment: 'master', // defaults to 'master' if not set
-    accessToken: 'O6DlrG-NCdovo84zhtSiMizryFQBUKCGTbElzbqdhfA'
+    accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN
   })
 
   export default async (req, res) => {
->>>>>>> solomon
    let richText = null
    try{
 
     await client.getEntries().then((entries) => {
         richText = entries.items
-<<<<<<< HEAD
-      })
-      .catch(console.error)
-     console.log(richText)
-     res.json({ message: "Mail sent successfully", richText });
-=======
         // console.log(`${richText}`)
       })
       .catch(console.error)
      console.log("hello world")
      res.json({ message: "Blog content fetched successfully", richText });
->>>>>>> solomon
    }catch (error) {
    console.error(error);
    res.status(500).json({ error: "Internal Server Error" });
