c631d9371e4a789ffae2d227222370bb9a769335
jupiterBill <techwizardhyped@gmail.com>
2023-11-01 21:22:00 +0100

    undo changes

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -4,9 +4,9 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { useEffect,useState } from "react";
 const contentful = require("contentful")
 const client = contentful.createClient({
-    space: process.env.NEXT_PUBLIC_SPACE_ID,
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
     environment: 'master', // defaults to 'master' if not set
-    accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
   })
 
   // export default async  (req, res) => {
