13e551621f16e17ddf89bba606dd4e091452e442
jupiterBill <techwizardhyped@gmail.com>
2023-11-15 12:35:48 +0100

    spinner

--- components/shared/Progress.jsx
+++ components/shared/Progress.jsx
@@ -7,14 +7,12 @@ const Progress = () => {
   
     return (
       <div className="flex items-center justify-center h-screen">
-       
-          <div className="bg-white p-8 rounded-lg shadow-lg">
-            
-            <p className="text-gray-700 mt-4">Loading...</p>
-            <Oval color="#00BFFF" height={80} width={80} />
-          </div>
-        
+      <div className="bg-white p-20 rounded-lg shadow-lg">
+        <p className="text-gray-700 text-3xl mt-6">Loading...</p>
+        <Oval color="#00BFFF" height={150} width={150} />
       </div>
+    </div>
+    
     );
   };
   
