7b1fbd9505fa0d3d159d31f3ccdeefedd9308403
Adeola David Adelakun <aadelakun28@gmail.com>
2023-11-16 23:48:32 +0100

    Fixed environment variable name error

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -7,9 +7,9 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { useRouter } from 'next/router';
 const contentful = require("contentful")
 const client = contentful.createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    space: process.env.CONTENTFUL_SPACE_ID,
     environment: 'master', // defaults to 'master' if not set
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
   })
 
 export default function Data({blogData}){
